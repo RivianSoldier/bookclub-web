b45e2fe2595ae5b7fd17f9784aa70667039e3c5d
RivianSoldier <bilzims.sg@gmail.com>
2023-08-01 21:16:50 -0300

    connection with api implemented and create user implemented

--- src/modules/unauthenticated/screens/Register/index.js
+++ src/modules/unauthenticated/screens/Register/index.js
@@ -1,11 +1,37 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Flex, Image, useToast } from '@chakra-ui/react'
 import { Text, Input, Link, Button } from 'components'
 import { useNavigate } from 'react-router-dom'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
+import { useMutation } from 'react-query'
+import { registerCall } from 'services/api/requests'
 
 export const RegisterScreen = () => {
   const navigate = useNavigate()
+  const toast = useToast()
+
+  const mutation = useMutation((newUser) => registerCall(newUser), {
+    onError: (error) => {
+      toast({
+        title: 'Falha ao criar a conta.',
+        description:
+          error?.response?.data?.error || 'Por favor, tente novamente.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true
+      })
+    },
+    onSuccess: () => {
+      toast({
+        title: 'Conta criada com sucesso.',
+        status: 'success',
+        duration: 6000,
+        isClosable: true
+      })
+      navigate('/')
+    }
+  })
+
   const { handleSubmit, values, handleChange, errors } = useFormik({
     initialValues: {
       name: '',
@@ -29,7 +55,7 @@ export const RegisterScreen = () => {
         .oneOf([Yup.ref('password')], 'As senhas não são iguais.')
     }),
     onSubmit: (data) => {
-      console.log({ data })
+      mutation.mutate(data)
     }
   })
 
@@ -85,7 +111,12 @@ export const RegisterScreen = () => {
             placeholder="Confirmar senha"
           />
 
-          <Button mb={['24px', '0px']} mt="24px" onClick={handleSubmit}>
+          <Button
+            isLoading={mutation.isLoading}
+            mb={['24px', '0px']}
+            mt="24px"
+            onClick={handleSubmit}
+          >
             Cadastrar
           </Button>
           <Link.Action
