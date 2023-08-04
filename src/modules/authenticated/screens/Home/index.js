08f76100217fbdc27d5f67a125f3dcadc02e2632
RivianSoldier <bilzims.sg@gmail.com>
2023-08-03 23:09:39 -0300

    highlighted books section created

--- src/modules/authenticated/screens/Home/index.js
+++ src/modules/authenticated/screens/Home/index.js
@@ -1,13 +1,32 @@
 import { Flex } from '@chakra-ui/react'
-// import { useSelector } from 'react-redux'
-import { NavBar } from 'components'
+import { NavBar, BookList } from 'components'
+import { useQuery } from 'react-query'
+import { getHighlightedBooks } from 'services/api/requests'
 
 export const HomeScreen = () => {
-  // const userStore = useSelector((state) => state.user)
+  const { error, data } = useQuery('highlighted', getHighlightedBooks)
+  console.log({ error, data })
 
   return (
-    <Flex>
+    <Flex flexDir="column">
       <NavBar />
+      <Flex
+        mt="48px"
+        h="200px"
+        w="100%"
+        paddingX={['24px', '48px', '80px', '112px']}
+      >
+        <Flex
+          h="100%"
+          w="100%"
+          backgroundImage="url(/img/banner.svg)"
+          backgroundSize="cover"
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+          borderRadius="24px"
+        />
+      </Flex>
+      <BookList data={data?.data} />
     </Flex>
   )
 }
